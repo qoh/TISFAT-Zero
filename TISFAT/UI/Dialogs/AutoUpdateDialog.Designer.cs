07ea58c44d95d3b97304103fe03897e152db8663
Evar Smith <Bxcar92@gmail.com>
2015-10-16 17:24:16 -0400

    Initial Auto Update Dialog

---
+++ TISFAT/UI/Dialogs/AutoUpdateDialog.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace TISFAT
+{
+	partial class AutoUpdateDialog
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutoUpdateDialog));
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(318, 29);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(116, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Update Now";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(318, 58);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(116, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Update on Close";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(318, 218);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(116, 23);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "No Thanks";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.richTextBox1.Location = new System.Drawing.Point(1, 1);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(298, 210);
+			this.richTextBox1.TabIndex = 3;
+			this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(300, 17);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "There\'s an update available for download!  Changes include:\r\n";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(325, 99);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(103, 26);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Your Version: v1.4.5\r\nNew Version: v1.4.5\r\n";
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+			this.panel1.Controls.Add(this.richTextBox1);
+			this.panel1.Location = new System.Drawing.Point(12, 29);
+			this.panel1.Name = "panel1";
+			this.panel1.Padding = new System.Windows.Forms.Padding(1);
+			this.panel1.Size = new System.Drawing.Size(300, 212);
+			this.panel1.TabIndex = 6;
+			// 
+			// AutoUpdateDialog
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(444, 251);
+			this.ControlBox = false;
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Name = "AutoUpdateDialog";
+			this.Text = "Auto Update";
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
